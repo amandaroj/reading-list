8e0d6dffe251784cb281d293273cff1913a779a1
amanda rojter <rojteramanda@gmail.com>
2023-03-03 13:05:14 -0500

    change route for devise sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'reviews/index'
-  # get 'reviews/show'
-  # get 'reviews/new'
-  # get 'reviews/edit'
-  # get 'reviews/create'
-  # get 'reviews/update'
-  # get 'reviews/destroy'
-  # get 'genres/index'
-  # get 'bookshelves/index'
-  # get 'bookshelves/show'
-  # get 'bookshelves/new'
-  # get 'bookshelves/edit'
-  # get 'bookshelves/create'
-  # get 'bookshelves/update'
-  # get 'bookshelves/destroy'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'pages#home'
   get 'genres/index'
-  resources :books, only: [:index, :show]
-  resources :authors, only: [:index, :show]
+  resources :books, only: %i[index show]
+  resources :authors, only: %i[index show]
   resources :bookshelves
   resources :reviews
 end
